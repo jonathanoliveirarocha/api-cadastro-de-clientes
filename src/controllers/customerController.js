69b7d4208889e66cef58919de868a2c01268ac38
Jonathan de Oliveira <jonathandeoliveirarocha2002@gmail.com>
2024-02-01 14:37:42 -0300

    Ending findonebycpf route

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,9 +1,32 @@
 const customerService = require("../services/customer.service");
+const verifyCpf = require("../helpers/verifyCpf");
 
 const customerController = {
   createCustomer: async (req, res) => {},
 
-  getCostumerByCpf: async (req, res) => {},
+  getCostumerByCpf: async (req, res) => {
+    const { cpf } = req.body;
+
+    try {
+      if (!verifyCpf(cpf)) {
+        res.status(400).json({ mensagem: "Invalid CPF!" });
+      } else {
+        
+        const customer = await customerService.findOneByCpf(verifyCpf(cpf));
+
+        if (!customer) {
+          res.status(404).json({ mensagem: "Customer not yet registered!" });
+        } else {
+          res.json(customer);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "An internal error occurred when querying CPF!" });
+    }
+  },
 
   getAllCostumersInRange: async (req, res) => {
     const start = parseInt(req.query.start) | 0;
@@ -14,7 +37,10 @@ const customerController = {
 
       res.json(customers);
     } catch (error) {
-      res.status(500).json({ error: "Error when looking for clients!" });
+      console.log(error);
+      res.status(500).json({
+        error: "An internal error occurred when looking for customers!",
+      });
     }
   },
 };
--- src/services/customer.service.js
+++ src/services/customer.service.js
@@ -3,7 +3,10 @@ const Customer = require("../models/Customer");
 const customerService = {
   createCustomer: async (customer) => {},
 
-  findOneByCpf: async (cpf) => {},
+  findOneByCpf: async (cpf) => {
+    const customer = await Customer.findOne({ cpf: cpf });
+    return customer;
+  },
 
   findAllInRange: async (start, end) => {
     const customers = await Customer.find()
